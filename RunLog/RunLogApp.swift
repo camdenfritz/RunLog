6ad2952116bfd9ee12d8cf19e7d3b09122c33fb5
camdenfritz <camden.fritz123@gmail.com>
2023-04-06 12:38:32 -0600

    Initial Commit

---
+++ RunLog/RunLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunLogApp.swift
+//  RunLog
+//
+//  Created by Camden Fritz on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RunLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
